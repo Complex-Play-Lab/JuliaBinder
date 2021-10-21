92613ce0999feea72e2dfb86db1d1f157088cf53
snotskie <snotskie@gmail.com>
2021-10-21 12:09:43 -0500

    Create config.jl

---
+++ config.jl
@@ -0,0 +1,12 @@
+using Pkg
+
+# Pkg.add("Colors")
+# Pkg.add("CSV")
+# Pkg.add("DataFrames")
+# Pkg.add("HypothesisTests")
+# Pkg.add("LinearAlgebra")
+# Pkg.add("MultivariateStats")
+# Pkg.add("Plots")
+# Pkg.add("Statistics")
+# Pkg.add("GLM")
+Pkg.add(url="https://github.com/snotskie/EpistemicNetworkAnalysis.jl")
