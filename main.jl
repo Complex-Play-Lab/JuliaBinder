6a6f6545241199f603ef315e43882cb85ac6d38e
snotskie <snotskie@gmail.com>
2021-10-21 13:46:13 -0500

    Create main.jl

---
+++ main.jl
@@ -0,0 +1,8 @@
+using EpistemicNetworkAnalysis
+using DataFrames
+using CSV
+using Plots
+
+# binder plot error fix, see: https://discourse.julialang.org/t/generation-of-documentation-fails-qt-qpa-xcb-could-not-connect-to-display/60988
+ENV["GKSwstype"] = "100"
+
